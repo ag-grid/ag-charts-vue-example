6a4e2710fc78c9d40946f93de8ecc93dc70c8322
Sean Landsman <sean@thelandsmans.com>
2020-01-16 19:28:47 +0000

    AG-3762 Framework Wrappers for Charts

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
-    <ag-charts-vue></ag-charts-vue>
+    <div>
+        <div>
+            <p>Marker Size: {{markerSize}}</p>
+            <button @click="setMarkerSize(25)">Reset Marker Size</button>
+            <button @click="setMarkerSize(markerSize - 1)">Decrease Marker Size</button>
+            <button @click="setMarkerSize(markerSize + 1)">Increase Marker Size</button>
+        </div>
+        <ag-charts-vue :options="options"></ag-charts-vue>
+    </div>
 </template>
 <script>
     import {AgChartsVue} from "ag-charts-vue";
 
     export default {
         data() {
-            return {}
+            return {
+                markerSize: 25,
+                options: {}
+            }
         },
         components: {
             AgChartsVue,
         },
-        methods: {},
+        methods: {
+            setMarkerSize(markerSize) {
+                this.markerSize = markerSize;
+                this.updateOptions();
+            },
+            updateOptions() {
+                this.options = {
+                    data: [{
+                        month: 'Jan',
+                        revenue: 155000,
+                        profit: 33000
+                    }, {
+                        month: 'Feb',
+                        revenue: 123000,
+                        profit: 35500
+                    }, {
+                        month: 'Mar',
+                        revenue: 172500,
+                        profit: 41000
+                    }, {
+                        month: 'Apr',
+                        revenue: 185000,
+                        profit: 50000
+                    }],
+                    series: [{
+                        xKey: 'month',
+                        yKey: 'revenue'
+                    }],
+                    legend: {
+                        markerSize: this.markerSize
+                    }
+                };
+            }
+        },
         beforeMount() {
+            this.updateOptions();
         }
     }
 </script>
